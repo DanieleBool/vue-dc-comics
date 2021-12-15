5b0e655a9ae3c4dc0de1ec5b532fbfec17bad070
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-12-15 20:04:42 +0100

    fine esercizio

---
+++ src/components/sections/jumbo.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <div class="jumbotron">
+            <img src="../../assets/img/jumbotron.jpg" alt="">
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'jumbo'
+}
+</script>
+
+<style lang = "scss" scoped>
+    section {
+        height: 320px;
+        .jumbotron {
+            height: 100%;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: top;
+            }
+        }
+    }
+</style>
